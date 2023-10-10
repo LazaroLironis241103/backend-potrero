0e695ee1cd740fb2d8f80ee84fe353ba3239b59b
Lazaro Lironis <99282167+LazaroLironis241103@users.noreply.github.com>
2023-10-10 17:09:51 -0300

    Add files via upload

--- clase6/recepcion.php
+++ clase6/recepcion.php
@@ -15,29 +15,29 @@ function asignarCalificacion($nota) {
     }
 }
 
-// verificar si se ha enviado el formulario
+
 if (isset($_GET['submit'])) {
-    // obtener los valores de los campos
+    
     $nombre = $_GET['nombreUsuario'];
     $nota = $_GET['notaNumerica'];
-    // validar que el nombre exista y no esté vacío
+
     if (!isset($nombre) || empty($nombre)) {
         echo "ERROR! El nombre del alumno no fue ingresado.";
         exit;
     }
-    // validar que la nota exista y no esté vacía
+
     if (!isset($nota) || empty($nota)) {
         echo "ERROR! La nota numérica no fue ingresada.";
         exit;
     }
-    // validar que la nota sea un número entre 0 y 10
+
     if (!is_numeric($nota) || $nota < 0 || $nota > 10) {
         echo "ERROR! La nota numérica no es válida.";
         exit;
     }
-    // asignar una calificación según la nota
+
     $calificacion = asignarCalificacion($nota);
-    // mostrar el mensaje con el nombre y la calificación
+
     echo "El alumno $nombre tiene una calificación de $calificacion.";
 }
 
